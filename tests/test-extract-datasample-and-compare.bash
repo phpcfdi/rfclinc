e82f0838910287cade507eb2551e2f3071c8fc1d
Carlos C Soto <eclipxe13@gmail.com>
2018-03-20 17:17:50 -0600

    in test cli obtain dependencies using which

--- tests/test-extract-datasample-and-compare.bash
+++ tests/test-extract-datasample-and-compare.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-TESTPATH="$(dirname $0)"
+# dependencies
+GUNZIP="$(which gunzip)"
+OPENSSL="$(which openssl)"
+ICONV="$(which iconv)"
+SED="$(which sed)"
+TAIL="$(which tail)"
+DIFF="$(which diff)"
+
+TESTPATH="$(dirname "$0")"
 INFILE="$TESTPATH/assets/datasample-signed.txt.gz"
 UNCOMPRESSED="$(mktemp --dry-run)"
 UNPACKED="$(mktemp --dry-run)"
@@ -9,31 +17,31 @@ CLEANED="$(mktemp --dry-run)"
 
 EXPECTED="$TESTPATH/assets/datasample-contents.txt"
 
-cat "$INFILE" | /bin/gunzip --stdout > "$UNCOMPRESSED"
+cat "$INFILE" | "$GUNZIP" --stdout > "$UNCOMPRESSED"
 if [ 0 -ne $? ]; then
     echo "Error while gunzip" 1>&2
     exit 1
 fi
 
-cat "$UNCOMPRESSED" | /usr/bin/openssl smime -verify -in - -inform der -noverify 2> /dev/null > "$UNPACKED"
+cat "$UNCOMPRESSED" | "$OPENSSL" smime -verify -in - -inform der -noverify > "$UNPACKED"
 if [ 0 -ne $? ]; then
     echo "Error while openssl smime" 1>&2
     exit 1
 fi
 
-cat "$UNPACKED" | /usr/bin/iconv --from iso8859-1 --to utf-8 > "$ENCODED"
+cat "$UNPACKED" | "$ICONV" --from iso8859-1 --to utf-8 > "$ENCODED"
 if [ 0 -ne $? ]; then
     echo "Error while iconv" 1>&2
     exit 1
 fi
 
-cat "$ENCODED" | /bin/sed 's/\r$//' > "$CLEANED"
+cat "$ENCODED" | "$SED" 's/\r$//' > "$CLEANED"
 if [ 0 -ne $? ]; then
     echo "Error while sed" 1>&2
     exit 1
 fi
 
-sed -i '1,2d' "$CLEANED"
+"$SED" -i '1,2d' "$CLEANED"
 diff "$CLEANED" "$EXPECTED" > /dev/null
 if [ 0 -ne $? ]; then
     echo "The file $CLEANED does not match with $EXPECTED (step by step)" 1>&2
@@ -42,14 +50,14 @@ fi
 
 
 OUTFILE="$(mktemp --dry-run)"
-/bin/gunzip --stdout "$INFILE" \
-    | /usr/bin/openssl smime -verify -in - -inform der -noverify 2> /dev/null \
-    | /usr/bin/iconv --from iso8859-1 --to utf-8 \
-    | /bin/sed 's/\r$//' \
+"$GUNZIP" --stdout "$INFILE" \
+    | "$OPENSSL" smime -verify -in - -inform der -noverify 2> /dev/null \
+    | "$ICONV" --from iso8859-1 --to utf-8 \
+    | "$SED" 's/\r$//' \
     > "$OUTFILE"
 
-sed -i '1,2d' "$OUTFILE"
-diff "$OUTFILE" "$EXPECTED" > /dev/null
+"$SED" -i '1,2d' "$OUTFILE"
+"$DIFF" "$OUTFILE" "$EXPECTED" > /dev/null
 if [ 0 -ne $? ]; then
     echo "The file $OUTFILE does not match with $EXPECTED (in one line)" 1>&2
     exit 1
